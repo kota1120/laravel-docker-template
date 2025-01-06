4d0bf6cdfd1f2934b8515b0f82a96d673604ee8d
kota1120 <11kota20s@gmail.com>
2025-01-06 15:22:00 +0900

    add: バリデーション

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use App\Http\Requests\TodoRequest;
 use App\Todo;
 
 class TodoController extends Controller
@@ -27,7 +26,7 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
         $this->todo->fill($inputs);
@@ -50,11 +49,12 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]);
     }
 
-    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
     {
         $inputs = $request->all();
         $todo = $this->todo->find($id);
-        $todo->fill($inputs)->save();
+        $todo->fill($inputs);
+        $todo->save();
 
         return redirect()->route('todo.show', $todo->id);
     }
